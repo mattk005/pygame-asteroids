b55ef4a98abd3296fd0a0e1c7ee2ca5ef3d8c48e
mk005 <mattydogs@gmail.com>
2025-12-13 19:39:30 -0800

    moved player into groups 'updatable' and 'drawable'

--- main.py
+++ main.py
@@ -4,24 +4,24 @@ from constants import PLAYER_RADIUS, SCREEN_WIDTH, SCREEN_HEIGHT
 from player import Player
 
 
-# def game_loop():
-# print(dt)
-
-
 def main():
     pygame.init()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
-
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        player.draw(screen)
+        updatable.update(dt)
+        for _drawable in drawable:
+            _drawable.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
