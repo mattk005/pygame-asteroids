f5b52a62305e8d11c2078952d2333d48ca1ba6ad
mk005 <mattydogs@gmail.com>
2025-12-15 23:02:07 -0800

    added shots and collisons

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import sys
 import pygame
 from asteroidfield import AsteroidField
 from asteroid import Asteroid
+from shot import Shot
 from logger import log_state, log_event
 from constants import PLAYER_RADIUS, SCREEN_WIDTH, SCREEN_HEIGHT
 from player import Player
@@ -15,10 +16,12 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
 
     Player.containers = (updatable, drawable)
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = updatable
+    Shot.containers = (shots, updatable, drawable)
 
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     astroid_field = AsteroidField()
@@ -33,6 +36,12 @@ def main():
         updatable.update(dt)
         screen.fill("black")
         for _asteroid in asteroids:
+            for _shot in shots:
+                if _asteroid.collinds_with(_shot):
+                    log_event("asteroid_shot")
+                    _asteroid.kill()
+                    _shot.kill()
+
             if _asteroid.collinds_with(player):
                 log_event("player_hit")
                 print("Game over!")
